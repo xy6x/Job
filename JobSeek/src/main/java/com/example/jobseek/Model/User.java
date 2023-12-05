520db08f91c7fbf3332b4bf4d613f4b41c3fab5c
yousef_Al-anzi <121990181+xy6x@users.noreply.github.com>
2023-12-06 00:30:47 +0300

    Add files via upload

--- JobSeek/src/main/java/com/example/jobseek/Model/User.java
+++ JobSeek/src/main/java/com/example/jobseek/Model/User.java
@@ -2,6 +2,8 @@ package com.example.jobseek.Model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,14 +17,19 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id ;
+    @NotEmpty(message = "please enter name")
     @Column(columnDefinition = "varchar(50) not null")
     private String name;
+    @NotEmpty(message = "please enter email ")
     @Column(columnDefinition = "varchar(30) not null unique")
     private String email;
+    @NotEmpty(message = "please enter password")
     @Column(columnDefinition = "varchar(50) not null")
     private String password;
+    @NotNull(message = "please enter age")
     @Column(columnDefinition = "int(45) not null ")
     private  Integer age;
+    @NotEmpty(message = "please enter JOB_SEEKER or EMPLOYER")
     @Column(columnDefinition = "varchar(255) not null check(role ='JOB_SEEKER' or role ='EMPLOYER')")
     private String role;
 }
